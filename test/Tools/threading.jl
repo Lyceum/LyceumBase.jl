a6c9eb21601f642ec5563deb2d3222a8874383c6
colinxs <me@colinxsummers.com>
2020-01-14 18:38:01 -0800

    update threading tests

--- test/Tools/threading.jl
+++ test/Tools/threading.jl
@@ -1,36 +1,54 @@
-let N = 16, np = 4
-    x = splitrange(N, np)
-    @test x == (1:4, 5:8, 9:12, 13:16)
-    for i = 1:np
-        @test x[i] == getrange(N, np, i)
+@testset "splitrange/getrange" begin
+    let N = 16, np = 4
+        x = splitrange(N, np)
+        @test x == (1:4, 5:8, 9:12, 13:16)
+        for i = 1:np
+            @test x[i] == getrange(N, np, i)
+        end
+    end
+
+    let N = 4, np = 16
+        x = splitrange(N, np)
+        @test x == (1:1, 2:2, 3:3, 4:4)
+        for i = 1:N
+            @test x[i] == getrange(N, np, i)
+        end
     end
 end
 
-let N = 4, np = 16
-    x = splitrange(N, np)
-    @test x == (1:1, 2:2, 3:3, 4:4)
-    for i = 1:N
-        @test x[i] == getrange(N, np, i)
+@testset "seed_threadrngs!" begin
+    x = zeros(1000)
+    y = zeros(1000)
+    seed_threadrngs!(1)
+    Threads.@threads for i=1:length(x)
+       y[i] = rand()
     end
-end
+    seed_threadrngs!(1)
+    Threads.@threads for i=1:length(x)
+       x[i] = rand()
+    end
+    @test x == y
 
-Threads.@threads for _ = 1:Threads.nthreads()
+    states = map(rng -> rng.state, Random.THREAD_RNGs)
+    @test length(unique(states)) == Threads.nthreads()
+
+    # Test that naievely calling Random.seed! in a single thread doesn't
+    # seed all threads
+    x = zeros(1000)
+    y = zeros(1000)
     Random.seed!(1)
-end
-let states = map(rng -> rng.state, Random.THREAD_RNGs)
-    @test length(unique(states)) == 1
-end
-seed_threadrngs!(1)
-let states = map(rng -> rng.state, Random.THREAD_RNGs)
-    @test length(unique(states)) == Threads.nthreads()
-end
-
-let rngs = threadrngs()
-    states = map(rng -> rng.state, rngs)
-    @test length(unique(states)) == Threads.nthreads()
+    Threads.@threads for i=1:length(x)
+       y[i] = rand()
+    end
+    Random.seed!(1)
+    Threads.@threads for i=1:length(x)
+       x[i] = rand()
+    end
+    @test x != y
 end
 
-let nt = Threads.nthreads()
+@testset "nblasthreads/with_blasthreads" begin
+    nt = Threads.nthreads()
     if nt > 1
         nb = min(16, nt) # Julia's OpenBLAS allows a max of 16 threads
         BLAS.set_num_threads(nb)
